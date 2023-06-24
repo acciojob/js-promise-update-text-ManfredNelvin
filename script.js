c2a1715011dcef84931b784a4310fa6526b9bf0b
ManfredNelvin <119521670+ManfredNelvin@users.noreply.github.com>
2023-06-24 15:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function updateElementWithHelloWorld() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+// Call the function and update the HTML element
+updateElementWithHelloWorld()
+  .then((message) => {
+    const element = document.getElementById('output');
+    element.textContent = message;
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
+
